78718bfa3f0f3f69d1e4f3271697e8ff59e31cdf
kom-senapati <komnoob123@gmail.com>
2025-03-14 19:48:59 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center text-4xl font-bold text-orange-600 bg-orange-200">
+      🔥 Cooking
+    </div>
+  )
+}
